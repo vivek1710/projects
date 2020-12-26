7a6cec3da99365f265947f88b45fc6a05f559bb2
VivekSantani <70746339+VivekSantani@users.noreply.github.com>
2020-12-26 20:17:43 +0530

    portfolio

--- app.js
+++ app.js
@@ -1,36 +1,28 @@
-//movement 
-const card = document.querySelector (".card");   
-const container = document.querySelector(".container");
-//animate
-const title = document.querySelector(".title")
-const purchase = document.querySelector(".purchase button")
-const sizes = document.querySelector(".sizes ")
-const sneaker = document.querySelector(".sneaker img")
+const navSlide = ()=>{
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const links = document.querySelectorAll('.nav-links li');
 
+    //Toggle nav
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle('nav-active')
 
-container.addEventListener('mousemove', e => {
-    let xAxis = (window.innerWidth / 2 - e.pageX)/10;
-    let yAxis = (window.innerHeight / 2 - e.pageY)/10;
-    card.style.transform = `rotateY(${xAxis}deg)  rotateX(${yAxis}deg)`;
-});
+        links.forEach((link, index) => {
+            if(link.style.animation)
+            {
+                link.style.animation = '';
+            }
+            else
+            {
+            link.style.animation   = `navLinksFade 0.5s ease forwards ${index /5 +0.5}s`
+            }
+        });
 
-//Animate In
-container.addEventListener("mouseenter" , e => {
-    card.style.transition = `none`
-//PopOut
-    title.style.transform = "translateZ(50px)";
-    purchase.style.transform = "translateZ(20px)";
-    sizes.style.transform = "translateZ(30px)";
-    sneaker.style.transform = "translateZ(100px) rotateZ(-45deg)";
+        burger.classList.toggle('toggle')
+    });
 
-})
+    //Animate fade
+    
+};
 
-//Animate Out
-container.addEventListener("mouseleave" , e => {
-    card.style.transform = `rotateY(0deg)  rotateX(0deg)`;
-    card.style.transition = `all 0.5s ease`;
-    title.style.transform = "translateZ(0px)";
-    purchase.style.transform = "translateZ(0px)";
-    sizes.style.transform = "translateZ(0px)";
-    sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
-})
+navSlide();
